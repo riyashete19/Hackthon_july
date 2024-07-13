1028d04d4493a76c360fd327afcc463cd3618012
omghante <mr.omghante1@gmail.com>
2024-07-13 07:35:22 +0530

    Another Commit

--- 07-2024/backend/index.js
+++ 07-2024/backend/index.js
@@ -13,6 +13,14 @@ app.use(cors({
     credentials: true
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://july-hackthon-frontend.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
