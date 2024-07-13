dc8a7c76e70a18980d3dd2bbd1889a64a3b576aa
omghante <mr.omghante1@gmail.com>
2024-07-13 07:21:04 +0530

    Another Commit

--- 07-2024/backend/routes/authRoutes.js
+++ 07-2024/backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-router.post("/register", async (req, res) => {
+router.post("/user/data", async (req, res) => {
     const { uerfullname, useremail, userphone, usercreatedpass, userfinalpass } = req.body;
 
     if (usercreatedpass !== userfinalpass) {
